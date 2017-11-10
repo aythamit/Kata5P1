6b9a8c84f1f3843b99961c79697264ecb335d961
Entrar <Entrar@APD-L13>
2017-11-10 13:31:45 +0000

    Tercer Commit

--- src/Controlador/Kata5P1.java
+++ src/Controlador/Kata5P1.java
@@ -1,15 +1,20 @@
 
-package kata5p1;
+package Controlador;
 
+import Modelo.Mail;
+import Vista.MailListReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
+        String fileName = "emails.txt";
         Class.forName("org.sqlite.JDBC");
         
         Connection connection = DriverManager.getConnection("jdbc:sqlite:KATA5.db");
@@ -22,7 +27,17 @@ public class Kata5P1 {
         }
          
         statement.execute("CREATE TABLE IF NOT EXISTS MAIL ('ID' INTEGER PRIMARY KEY AUTOINCREMENT, 'Mail' TEXT NOT NULL)");
-
+        
+        
+        ArrayList<Mail> mails = new ArrayList<Mail>();
+        mails = (ArrayList<Mail>) MailListReader.read(fileName);
+        
+        String query;
+        for (Mail it : mails) {
+            query = "INSERT INTO MAIL (Mail) VALUES ('" + it + "');";
+            statement.executeUpdate(query);
+        }
+        
     }
     
 }
